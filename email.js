0227b67180c53e3632b7f7cf6b31d6de80f2ca92
Mohammad Solaiman <solaiman321@gmail.com>
2021-05-14 13:11:08 +0600

    done

--- email.js
+++ email.js
@@ -12,7 +12,7 @@ const sendEmail = async (name,message) => {
 
 
 const msg = {
-    to:"solaiman321@gmail.com",
+    to:"small.talk.confidence@gmail.com",
     from:"small.talk.confidence@gmail.com",
     subject:`${name} - Small Talk sentences`,
     text: message,
