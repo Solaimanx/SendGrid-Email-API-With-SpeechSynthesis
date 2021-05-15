7243ccb3f594af6f65ccb2fc7defa02509a5f948
Mohammad Solaiman <solaiman321@gmail.com>
2021-05-15 16:05:59 +0600

    added var for the veriable

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.get("/", function (req, res) {
   res.send("Access Denied");
 });
 
-cosnt
+
 
 app.post("/api", async function (req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "*");
