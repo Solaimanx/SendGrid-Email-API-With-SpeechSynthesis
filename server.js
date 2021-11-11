8e0688edb76f5610175b56948d3ac2dbf2e6da1a
Mohammad Solaiman <solaiman321@gmail.com>
2021-11-11 19:02:31 +0600

    fixed schedule issue

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const express = require("express");
 const app = express();
 
 const schedule = require("node-schedule");
+const sgMail = require("@sendgrid/mail");
 
+sgMail.setApiKey(process.env.NEWAPI);
 
 
 const cors = require("cors");
@@ -47,23 +49,133 @@ app.get('/api/forgot-password/:rawemail',(req,res,next) => {
 
   const { rawemail } = req.params
 
-  const currectTime = new Date().getTime() + 1 * 60 * 1000;
+  const currectTime = new Date().getTime() + 5 * 60 * 1000;
   const waiting = new Date(currectTime);
 
 
   schedule.scheduleJob(waiting, async function () {
     const email = rawemail;
-    console.log(email)
+
+
+    const msg = {
+      to: email,
+      from: " אריאל אפל (קונפידנס לימוד אנגלית) <info@english21days.co.il>",
+      subject: `פרטי הגישה שלך `,
+
+      html: `
+    <table style="  direction:rtl ; text-align:right ; width:100%;border-spacing:0px;border-collapse:collapse;border-width:medium;border-style:none" role="presentation">
+                                  <tbody>
+                                    <tr>
+                                      <td style="padding:0px;width:100%" width="100%" valign="top">
+                                        <div>
+                                          <div style="font-size:16px">
+                                            <div>
+                                              <div>הי!</div>
+                                              <div><br>
+                                                לאחרונה ביקשת לשנות
+                                                סיסמא לחשבונך.<br>
+                                                <br>
+                                                במידה וכבר שינית את
+                                                הסיסמא, <br>
+                                                פשוט תיכנס מכאן עם המייל
+                                                והסיסמא שיצרת:<br>
+                                                <a href="https://www.english21days.co.il/course33523349" target="_blank" data-saferedirecturl="https://www.google.com/url?q=https://www.english21days.co.il/course33523349&amp;source=gmail&amp;ust=1636575999458000&amp;usg=AFQjCNEF7xl87vTFe_um9yW-jR9TvcS_Vg">https://www.english21days.co.<wbr>il/course33523349</a><br>
+                                                (כדאי לשמור את הקישור
+                                                הזה במועדפים שיהיה לך קל
+                                                למצוא)<br>
+                                                <br>
+                                                במידה ועדיין לא שינית את
+                                                הסיסמא,<br>
+                                                ניתן לעשות זאת <a href="https://www.english21days.co.il/thank-you-basic-plankz1yvwq9" target="_blank" data-saferedirecturl="https://www.google.com/url?q=https://www.english21days.co.il/thank-you-basic-plankz1yvwq9&amp;source=gmail&amp;ust=1636575999458000&amp;usg=AFQjCNF3Ig2WYhHdQCSwVc63BWr3xE7OeA">מכאן</a>.<br>
+                                              </div>
+                                              <div><br>
+                                              </div>
+                                              <div><br>
+                                                מאחל לך לימוד נעים
+                                                ומהנה,<br>
+                                                אריאל</div>
+                                              <div>&nbsp;</div>
+                                            </div>
+                                          </div>
+                                        </div>
+                                        <div><span>
+                                            <table style="float:none;text-align:right;border-spacing:0px;border-collapse:collapse;border-width:medium;border-style:none" role="presentation" width="100%" align="right">
+                                              <tbody>
+                                                <tr>
+                                                  <td style="padding:0px 0px 0px 0px" align="right"> <img src="https://ci6.googleusercontent.com/proxy/NFq5F8FXTyJM6yeepc3Yca4Wo9kAasjJH5cknq06Iay5PluCdAi795i2M8R2oazXeMZqekoKLOcOkORHaj7eedPPJUVIvc4AolyJdgerYxFbZkcZwd6sCUmMn_MQ_n_r8mWa52APVI_YWSoXCWDyArOoQijTwf59yQ=s0-d-e1-ft#https://hostedimages-cdn.aweber-static.com/MTAyNTkyMA==/thumbnail/697ac1bb04e54e1f87d58b5e3a11c7b4.png" style="display:block;width:136px;height:150px;max-width:100%" alt="" width="136" height="150" class="CToWUd"> </td>
+                                                </tr>
+                                              </tbody>
+                                            </table>
+                                          </span></div>
+                                        <div>
+                                          <div style="font-size:16px">
+                                            <div>
+                                              <div>&nbsp;</div>
+                                              <div>&nbsp;</div>
+                                              <div>&nbsp;</div>
+                                              <div>&nbsp;</div>
+                                              <div>&nbsp;</div>
+                                              <div>&nbsp;</div>
+                                              <div>&nbsp;</div>
+                                              <div>&nbsp;</div>
+                                              <div>&nbsp;</div>
+                                              <div>&nbsp;</div>
+                                              <div>&nbsp;</div>
+                                              <div>&nbsp;</div>
+                                              <div>&nbsp;</div>
+                                              <div>&nbsp;</div>
+                                              <div>&nbsp;</div>
+                                              <div>&nbsp;</div>
+                                              <div>&nbsp;</div>
+                                              <div>&nbsp;</div>
+                                              <div>&nbsp;</div>
+                                              <div>&nbsp;</div>
+                                              <div>&nbsp;</div>
+                                              <div>&nbsp;</div>
+                                              <div>&nbsp;</div>
+                                              <div>&nbsp;</div>
+                                              <div>&nbsp;</div>
+                                              <div>&nbsp;</div>
+                                              <div>&nbsp;</div>
+                                              <div>&nbsp;</div>
+                                            </div>
+                                          </div>
+                                        </div>
+                                      </td>
+                                    </tr>
+                                  </tbody>
+                                </table>
+    
+    `,
+    };
+
+
+    await sgMail.send(msg, function (err, info) {
+      if (err) {
+        console.log(`Email Not Sent Error Occured => ${err}`);  
+        console.log(email)
+      } else {
+        console.log(`Email was Sent`);
+      }
+    });
+
+    
+
+
+
+
+
   })
 
-  const job = schedule.scheduleJob('1 * * * *', function(){
-    console.log('The answer to life, the universe, and everything!');
-  });
+
 
 console.log('complete')
 
 
+
   next()
+  return res.sendStatus(200).json({message : 'success'})
+
 })
 
 
